ecea7cd5e1eb464d0ba08634d5903d7cb607e037
Fluidone Morinmoto <morin@fluidone.it>
2020-01-29 19:15:13 +0100

    Added asterischi to the standard output

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
+echo "*****************************"
 echo "Hello World!";
 echo "\n";
+echo "*****************************"
 echo "\n";
 $version = file_get_contents("version.txt");
 echo "Version: $version";
