835452aeb831e905693e19a9ecd9408c9db7fa8e
Fluidone Morinmoto <morin@fluidone.it>
2020-01-29 19:19:33 +0100

    Added more info

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 echo "Hello World!";
 echo "\n";
 echo "\n";
+echo "Scriviamo ulteriori informazioni perche' lo vuole il cliente\n\n";
 $version = file_get_contents("version.txt");
 echo "Version: $version";
 ?>
