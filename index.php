fa6e7231e53dcbe71beb2081d25d0af1953adea4
Fluidone Morinmoto <morin@fluidone.it>
2020-01-30 18:06:57 +0100

    Added cancelletti at the top and at the bottom of output

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
 
-echo "*****************************\n";
+function printChar($char) {
+    for ($i = 0; $i < 29; $i++) {
+        echo $char;
+    }
+    echo "\n";
+}
+
+printChar("#");
+printChar("*");
+
 echo "Hello World!";
 echo "\n";
-echo "*****************************\n";
+
+printChar("*");
 echo "\n";
 echo "Scriviamo ulteriori informazioni perche' lo vuole il cliente\n\n";
 $version = file_get_contents("version.txt");
 echo "Version: $version";
+printChar("#");
 ?>
